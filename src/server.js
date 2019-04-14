146e083d85aa3f8a8f65701e0ce4900bf0751afb
Itamar Rocha <itamarrochaa@yahoo.com>
2019-04-14 11:56:38 -0300

    Adicionando socket io

--- src/server.js
+++ src/server.js
@@ -7,6 +7,15 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+
+io.on("connection", socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+});
 
 
 mongoose.connect('mongodb+srv://omnistack:AdminSemSenha@cluster0-oqpaq.mongodb.net/omnistack?retryWrites=true',
@@ -16,10 +25,16 @@ mongoose.connect('mongodb+srv://omnistack:AdminSemSenha@cluster0-oqpaq.mongodb.n
 ); 
 
 
+app.use((req,res,next) => {
+    req .io = io;
+    return next();
+});
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/files', express.static(path.resolve(__dirname,"..","tmp")));
 
 app.use(require('./routes'));
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
