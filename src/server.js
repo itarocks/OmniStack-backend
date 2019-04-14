f1bd2e4d73a5fb1522beb161ec5042173c95bc3e
Itamar Rocha <itamarrochaa@yahoo.com>
2019-04-14 10:42:21 -0300

    Retirando o socket .io

--- src/server.js
+++ src/server.js
@@ -5,17 +5,7 @@ const path = require('path');
 //Comandos do terminal 
 
 const app = express();
-const server = require('http').Server(app);
-const io = require("socket.io")(server);
 
-io.on("connection", socket =>
-{
-
-    socket.on('connectRoom', box =>{
-        socket.join(box);
-    })
-
-});
 
 mongoose.connect('mongodb+srv://omnistack:AdminSemSenha@cluster0-oqpaq.mongodb.net/omnistack?retryWrites=true',
 {
@@ -23,9 +13,6 @@ mongoose.connect('mongodb+srv://omnistack:AdminSemSenha@cluster0-oqpaq.mongodb.n
 }
 ); 
 
-app.use((req, res,next) => {
-    req.io = io;
-});
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -33,4 +20,4 @@ app.use('/files', express.static(path.resolve(__dirname,"..","tmp")));
 
 app.use(require('./routes'));
 
-server.listen(process.env.PORT || 3333);
+app.listen(process.env.PORT || 3333);
